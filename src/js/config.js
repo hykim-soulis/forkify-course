d84b3691e2286939537908c0f789e85f827f30f4
hykim-soulis <hyeyoung9002@gmail.com>
2022-03-09 10:53:58 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '61c193a6-15a4-4e2d-9aa0-beab35898c55';
+export const MODAL_CLOSED_SEC = 2.5;
